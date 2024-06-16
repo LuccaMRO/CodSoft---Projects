eba2597fd581874ace110780c3724afccc5df70d
LuccaMRO <mro.lucca@gmail.com>
2024-06-16 16:52:51 -0300

    Finishing the first part

--- Titanic/Titanic_Model.py
+++ Titanic/Titanic_Model.py
@@ -60,3 +60,46 @@ train_df['Age_cut'] = pd.qcut(train_df['Age'],8)
 test_df['Age_cut'] = pd.qcut(test_df['Age'],8)
 
 print(train_df.groupby(['Age_cut'], as_index=False)['Survived'].mean())
+
+train_df.loc[train_df['Age'] <= 16, 'Age'] = 0
+train_df.loc[(train_df['Age'] > 16) & (train_df['Age'] <= 20), 'Age'] = 1
+train_df.loc[(train_df['Age'] > 20) & (train_df['Age'] <= 24), 'Age'] = 2
+train_df.loc[(train_df['Age'] > 24) & (train_df['Age'] <= 27), 'Age'] = 3
+train_df.loc[(train_df['Age'] > 27) & (train_df['Age'] <= 34), 'Age'] = 4
+train_df.loc[(train_df['Age'] > 34) & (train_df['Age'] <= 42), 'Age'] = 5
+train_df.loc[(train_df['Age'] > 42) & (train_df['Age'] <= 51), 'Age'] = 6
+train_df.loc[train_df['Age'] > 51, 'Age'] = 7
+
+test_df.loc[test_df['Age'] <= 16, 'Age'] = 0
+test_df.loc[(test_df['Age'] > 16) & (test_df['Age'] <= 20), 'Age'] = 1
+test_df.loc[(test_df['Age'] > 20) & (test_df['Age'] <= 24), 'Age'] = 2
+test_df.loc[(test_df['Age'] > 24) & (test_df['Age'] <= 27), 'Age'] = 3
+test_df.loc[(test_df['Age'] > 27) & (test_df['Age'] <= 34), 'Age'] = 4
+test_df.loc[(test_df['Age'] > 34) & (test_df['Age'] <= 42), 'Age'] = 5
+test_df.loc[(test_df['Age'] > 42) & (test_df['Age'] <= 51), 'Age'] = 6
+test_df.loc[test_df['Age'] > 51, 'Age'] = 7
+
+sns.displot(train_df, x='Fare',col='Survived', binwidth=10, height=5)
+plt.show()
+
+train_df['Fare_cut'] = pd.qcut(train_df['Fare'],6)
+test_df['Fare_cut'] = pd.qcut(test_df['Fare'],6)
+
+print(train_df.groupby(['Fare_cut'], as_index=False)['Survived'].mean())
+
+train_df.loc[train_df['Fare'] <= 7.775, 'Fare'] = 0
+train_df.loc[(train_df['Fare'] > 7.775) & (train_df['Fare'] <= 8.662), 'Fare'] = 1 
+train_df.loc[(train_df['Fare'] > 8.662) & (train_df['Fare'] <= 14.454), 'Fare'] = 2
+train_df.loc[(train_df['Fare'] > 14.454) & (train_df['Fare'] <= 26), 'Fare'] = 3
+train_df.loc[(train_df['Fare'] > 26) & (train_df['Fare'] <= 52), 'Fare'] = 4
+train_df.loc[train_df['Fare'] > 52, 'Fare'] = 5
+
+test_df.loc[test_df['Fare'] <= 7.775, 'Fare'] = 0
+test_df.loc[(test_df['Fare'] > 7.775) & (test_df['Fare'] <= 8.662), 'Fare'] = 1
+test_df.loc[(test_df['Fare'] > 8.662) & (test_df['Fare'] <= 14.454), 'Fare'] = 2
+test_df.loc[(test_df['Fare'] > 14.454) & (test_df['Fare'] <= 26), 'Fare'] = 3
+test_df.loc[(test_df['Fare'] > 26) & (test_df['Fare'] <= 52), 'Fare'] = 4
+test_df.loc[test_df['Fare'] > 52, 'Fare'] = 5
+
+train_df['Title'] = train_df['Name'].str.split(pat=',',expand=True)[1].str.split(pat='.',expand=True)[0].apply(lambda x: x.strip())
+print(train_df['Title'])
